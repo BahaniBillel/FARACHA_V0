50d430ac702743b87ca23313dc6bba2882ec1773
BahaniBillel <billel.bahani@gmail.com>
2023-03-29 19:49:37 +0100

    fixing key issue

--- components/header/Navigation.jsx
+++ components/header/Navigation.jsx
@@ -18,7 +18,10 @@ function Navigation() {
   return (
     <div className=" overflow-x-auto  snap-start flex flex-row space-x-6 md:space-x-10 h-10 bg-black items-center md:justify-center px-3">
       {Navs.map((nav) => (
-        <p className="font-light text-xs  md:text-sm text-white cursor-pointer whitespace-pre ">
+        <p
+          className="font-light text-xs  md:text-sm text-white cursor-pointer whitespace-pre "
+          key={nav}
+        >
           {nav}
         </p>
       ))}
